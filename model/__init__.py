11895dab1b2509b5032a2cec6e25be2e73d39ae6
drchetancreatioon <chetan11103@gmail.com>
2026-03-15 23:12:46 +0530

    Update __init__.py

--- model/__init__.py
+++ model/__init__.py
@@ -1,6 +1,6 @@
 # model/__init__.py
 
 from .ecg_model import ECGClassifier
-from .inference import get_prediction
+from .inference import predict_ecg  # Changed from get_prediction to predict_ecg
 
-__all__ = ["ECGClassifier", "get_prediction"]
+__all__ = ["ECGClassifier", "predict_ecg"]
